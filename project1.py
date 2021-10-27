6ed7801b623d42134b67006931bc454c6937d867
Mallikarjun Nagoju <mnagoju@innominds.com>
2021-10-27 15:32:41 +0530

    Created a Project to Find out the largest Value in given 3 digits

---
+++ project1.py
@@ -0,0 +1,10 @@
+a=int(input('Enter A: '))
+b=int(input('Enter B: '))
+c=int(input('Enter C: '))
+
+if(a>b and b>c):
+    print('A is largest')
+if(b>a and b>c):
+    print('B is largest')
+if(c>a and c>b):
+    print('C is largest')
